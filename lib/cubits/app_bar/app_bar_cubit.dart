49167fb0a7bf3066d241fc9a85a1b14f905f8b93
firdauscode <egarifs25@gmail.com>
2021-02-01 06:55:37 +0700

    fist commit

---
+++ lib/cubits/app_bar/app_bar_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'app_bar_state.dart';
+
+class AppBarCubit extends Cubit<double> {
+  AppBarCubit() : super(0);
+
+  void setOffset(double offset) => emit(offset);
+}
+
+
+// event ->bloc -> state
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_clone/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final double scrollOffst;
+
+  const CustomAppBar({
+    Key key,
+    this.scrollOffst = 0.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 24.0,
+      ),
+      color:
+          Colors.black.withOpacity((scrollOffst / 350).clamp(0, 1).toDouble()),
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+            const SizedBox(
+              width: 12.0,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _AppBarButton(
+                    title: 'TV Show',
+                    onTap: () => print('TV Shows'),
+                  ),
+                  _AppBarButton(
+                    title: 'Moviews',
+                    onTap: () => print('Moviews'),
+                  ),
+                  _AppBarButton(
+                    title: 'My List',
+                    onTap: () => print('My List'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  final String title;
+  final Function onTap;
+
+  const _AppBarButton({
+    Key key,
+    @required this.title,
+    @required this.onTap,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
